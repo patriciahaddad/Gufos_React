284c2d7b60b0ff0dea878aa549a4e7d81de7f6b6
Paulo Brandão <paulobrandaoofficial@gmail.com>
2019-11-26 12:56:39 -0200

    Eventos CRUD completo

--- src/pages/Eventos/Eventos.js
+++ src/pages/Eventos/Eventos.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import Cabecalho from '../../components/Cabecalho/Cabecalho';
 import Rodape from '../../components/Rodape/Rodape';
 import api from '../../services/api';
-import { MDBBtn, MDBAlert } from "mdbreact";
+import { MDBBtn, MDBInput, MDBAlert, MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter} from "mdbreact";
+
 
 export default class Eventos extends Component{
 
@@ -32,8 +33,19 @@ export default class Eventos extends Component{
             },
 
             erroMsg : "",
+            successMsg : "",
+            modal: false,
+
         }
 
+
+
+    }
+
+    toggle = () => {
+        this.setState({
+          modal: !this.state.modal
+        });
     }
 
     componentDidMount(){
@@ -41,6 +53,7 @@ export default class Eventos extends Component{
         this.getCategorias();
         this.getLocalizacao();
     }
+
 
     //#region GETs
     getEventos = () =>{
@@ -107,6 +120,84 @@ export default class Eventos extends Component{
     }
     //#endregion
 
+
+    //#region DELETE
+    deleteEvento(id){
+
+        this.setState({ successMsg : "" })
+
+        api.delete('/evento/'+id)
+        .then(response => {
+            if(response.status === 200){
+                this.setState({ successMsg : "Excluído com sucesso" })
+
+                setTimeout(() => {
+                    this.getEventos();
+                }, 1500);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            this.setState({ erroMsg : "Falha ao excluir" })
+        })
+        
+    }
+    //#endregion
+    
+    //#region 
+    putSetState = (input) =>{
+        this.setState({
+            putEvento : {
+                ...this.state.putEvento, [input.target.name] : input.target.value
+            }   
+        })
+    }
+    
+    putEvento = (event) =>{
+        
+        event.preventDefault();
+        let evento_id = this.state.putEvento.eventoId;
+        let evento_alterado = this.state.putEvento;
+        
+        // Converter para booleano
+        if(this.state.putEvento.acessoLivre === "0"){ 
+            this.state.putEvento.acessoLivre = false 
+        }else{
+            this.state.putEvento.acessoLivre = true
+        }
+        
+        api.put('/evento/'+evento_id, evento_alterado)
+        .then(() => {
+            this.setState({successMsg : "Evento alterado com sucesso!"});
+        })
+        .catch(error => {
+            console.log(error);
+            this.setState({erroMsg : "Falha ao alterar o Evento"});
+        })
+        
+        this.toggle();
+        
+        setTimeout(() => {
+            this.getEventos();
+        }, 1500);
+
+        setTimeout(() => {
+            this.setState({successMsg : ""});
+            this.setState({erroMsg : ""});
+        }, 3500);
+        
+    }
+    
+    openModal = (e) =>{
+        
+        this.toggle();
+        this.setState({putEvento : e});
+        
+        console.log("PUT", this.state.putEvento);
+    }
+    //#endregion
+    
+    
     render(){
        return(
            <>
@@ -116,6 +207,19 @@ export default class Eventos extends Component{
                 <section className="conteudoPrincipal-cadastro">
                     <h1 className="conteudoPrincipal-cadastro-titulo">Eventos</h1>
                     <div className="container" id="conteudoPrincipal-lista">
+                            {
+                                this.state.erroMsg && 
+                                <MDBAlert color="danger" >
+                                    {this.state.erroMsg}
+                                </MDBAlert>
+                            }
+                        
+                            {
+                            this.state.successMsg && 
+                                <MDBAlert color="success" >
+                                    {this.state.successMsg}
+                                </MDBAlert>
+                            }
                     <table id="tabela-lista">
                         <thead>
                         <tr>
@@ -145,17 +249,17 @@ export default class Eventos extends Component{
                                                 <td>{e.categoria.titulo}</td>
                                                 <td>{e.localizacao.endereco}</td>
                                                 <td>
-                                                    <MDBBtn color="primary" size="sm">
+                                                    <MDBBtn color="primary" size="sm" onClick={() => this.openModal(e)}>
                                                         <i className="fas fa-edit"></i>
                                                     </MDBBtn>
                                                     <br />
-                                                    <MDBBtn color="danger" size="sm">
+                                                    <MDBBtn color="danger" size="sm" onClick={() => this.deleteEvento(e.eventoId)}>
                                                         <i className="fas fa-trash"></i>
                                                     </MDBBtn>
                                                 </td>
                                             </tr>
                                         )
-                                    }
+                                    }.bind(this)
                                 )
                             }
                         </tbody>
@@ -236,12 +340,6 @@ export default class Eventos extends Component{
                     >
                         Cadastrar
                     </button>
-                    {
-                        this.state.erroMsg && 
-                        <MDBAlert color="danger" >
-                            {this.state.erroMsg}
-                        </MDBAlert>
-                    }
                     </div>
 
                     </form>
@@ -252,6 +350,90 @@ export default class Eventos extends Component{
 
 
             </main>
+
+            <MDBContainer>
+                <form onSubmit={this.putEvento}>
+                <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
+                <MDBModalHeader toggle={this.toggle}>Editar - {this.state.putEvento.titulo}</MDBModalHeader>
+                    <MDBModalBody>
+
+                        <MDBInput 
+                            label="Titulo" 
+                            name="titulo"
+                            value={this.state.putEvento.titulo}
+                            onChange={this.putSetState}
+                        />
+
+                        <MDBInput
+                            type="datetime-local"
+                            label="Data do Evento" 
+                            name="dataEvento"
+                            value={this.state.putEvento.dataEvento.split(",")[0]}
+                            onChange={this.putSetState}
+                        />
+
+                        <select 
+                            name="acessoLivre" 
+                            className="browser-default custom-select"
+                            onChange={this.putSetState}
+                        >
+                                <option value={this.state.putEvento.acessoLivre}>
+                                    {this.state.putEvento.acessoLivre == 0 && "Restrito"}
+                                    {this.state.putEvento.acessoLivre == 1 && "Livre"}
+                                </option>
+                                <option value="0">Restrito</option>
+                                <option value="1">Livre</option>
+                        </select>
+                        
+                        <select 
+                            name="categoriaId" 
+                            className="browser-default custom-select"
+                            onChange={this.putSetState}
+                            value={this.state.putEvento.categoriaId}
+                        >  
+                            {
+                                this.state.listaCategorias.map( function(c){
+                                    return(
+                                        <option 
+                                            key={c.categoriaId}
+                                            value={c.categoriaId}
+                                        >
+                                            {c.titulo}
+                                        </option>
+                                    )
+                                })
+                            }
+                        </select>
+
+                        <select 
+                            name="localizacaoId" 
+                            className="browser-default custom-select"
+                            onChange={this.putSetState}
+                            value={this.state.putEvento.localizacaoId}
+                        >  
+                            {
+                                 this.state.listaLocalizacao.map( function(l){
+                                    return(
+                                        <option 
+                                            key={l.localizacaoId}
+                                            value={l.localizacaoId}
+                                        >
+                                            {l.endereco}
+                                        </option>
+                                    )
+                                })
+                            }
+                        </select>
+
+                        
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                    <   MDBBtn color="secondary" onClick={this.toggle}>Fechar</MDBBtn>
+                        <MDBBtn color="primary" type="submit">Salvar</MDBBtn>
+                    </MDBModalFooter>
+                </MDBModal>
+                </form>
+            </MDBContainer>
 
             <Rodape />
            </>
